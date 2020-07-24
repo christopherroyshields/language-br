83622eea79515c1aeca588853f6fac212204ea64
Chris <christopherroyshields@gmail.com>
2020-07-24 00:08:53 -0500

    Added function for recursively searching parent directories for config file.  Added unit tests.

--- test/test.js
+++ test/test.js
@@ -4,41 +4,54 @@ const config = require('../lib/config.js');
 const path = require('path');
 
 describe('BR Language Support', function(){
-  describe('GetConfig', function(){
-    const testDir = "/test_project"
+  describe('Config', function(){
+    var
+      testDir,
+      testSubDir,
+      testSubSubDir,
+      configPath,
+      testconfig
+
     const configFilename = "br-project.json"
-    const configPath = path.join(testDir, configFilename)
 
-    const testconfig = JSON.stringify({
-      "layoutFolder": "filelay"
-    })
-
-    before(function(){
+    beforeEach(function(){
       // create test project
-      try {
-        fs.mkdirSync(testDir);
-      } catch(err){
-        if (err.name = "EEXIST"){
-          console.warn("WARNING: Test project already exists.")
-        }
-      }
+      testDir = fs.mkdtempSync(`${path.sep}test_project-`)
+      testSubDir = fs.mkdtempSync(path.join(testDir, "subdir-"))
+      testSubSubDir = fs.mkdtempSync(path.join(testSubDir, "subdir2-"))
+      configPath = path.join(testDir, configFilename)
+      const testconfig = JSON.stringify({
+        "layoutFolder": "filelay"
+      }, null, '\t')
 
       fs.writeFileSync(configPath, testconfig)
-
     })
 
-    it('should read and parse a config file.', async function(){
-      var testConfig = await config.Read(configPath)
-      assert.equal(testConfig.layoutFolder, "filelay")
-    })
-
-    after(function(){
+    afterEach(function(){
       try {
         fs.unlinkSync(configPath)
+        fs.rmdirSync(testSubSubDir)
+        fs.rmdirSync(testSubDir)
         fs.rmdirSync(testDir)
       } catch (err){
         console.warn("WARNING: Error removing test project.", configPath, testDir)
       }
     })
+
+    describe('Read Config', function(){
+      it('should read and parse a config file.', async function(){
+        var testConfig = await config.Read(configPath)
+        assert.equal(testConfig.layoutFolder, "filelay")
+      })
+    })
+
+    describe('Find Config', function(){
+      it("should walk up directory tree and find project file.", async function(){
+        var testConfig = await config.Find(testSubSubDir)
+        assert.equal(testConfig.layoutFolder, "filelay")
+      })
+    })
+
+
   })
 })
